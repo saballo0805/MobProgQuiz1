997dd6c10239ea7f97e7829429330d1ad054b7ae
saballo0805 <plsaballo08@gmail.com>
2023-08-30 17:14:34 +0800

    Files

---
+++ button2/app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View v) {
+
+        TextView text = (TextView) findViewById(R.id.textView1);
+        text.setText("Paul Lawrence Saballo");
+    }
+}
